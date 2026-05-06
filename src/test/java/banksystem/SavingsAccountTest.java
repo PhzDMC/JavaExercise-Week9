64dc891c57ea2d22c3fc6da86b7ec05a03485a1e
PhzDMC <tranhuuphuoc05082007zzz@gmail.com>
2026-05-06 20:25:17 +0700

    Test: Kiem tra hieu qua cua Maven cache

--- src/test/java/banksystem/SavingsAccountTest.java
+++ src/test/java/banksystem/SavingsAccountTest.java
@@ -10,7 +10,7 @@ public class SavingsAccountTest {
     java.io.File file = new java.io.File("folder\\file.txt");
 
     // Trên Windows, nó hiểu "folder" là thư mục cha.
-    // NHƯNG trên Linux, nó coi nguyên cả cụm "folder\file.txt" là 1 cái tên file, nên thư mục cha sẽ bị null.
+    // Nhưng trên Linux, nó coi nguyên cả cụm "folder\file.txt" là 1 cái tên file, nên thư mục cha sẽ bị null.
     org.junit.jupiter.api.Assertions.assertEquals("folder", file.getParent(), "Lỗi đường dẫn hệ điều hành!");
   }
   */
